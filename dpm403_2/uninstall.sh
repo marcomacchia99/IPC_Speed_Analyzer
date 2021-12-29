51abb55bde344e0d7f700cd324d1239ce819a61e
Marco <marcomacchia99@gmail.com>
2021-12-29 13:44:06 +0100

    change folders name. Edit uninstall.sh

--- dpm403_2/uninstall.sh
+++ dpm403_2/uninstall.sh
@@ -1,4 +1,11 @@
-cd -- "$(find -iname logs -type d)"
-cd ..
-rm -rf -- "$(pwd -P)"
-cd ..
+FOLDER="$(find -iname logs -type d)"
+
+if [ -z "${FOLDER}" ]
+then
+    echo "Source is not unzipped yet."
+else
+    cd "$FOLDER"
+    cd ..
+    rm -rf -- "$(pwd -P)"
+    cd ..
+fi
